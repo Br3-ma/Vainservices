17b1e27d9e52356bba9aa7d7f0b4bf0e7f899b25
br3-mah <nyeleti.bremah@gmail.com>
2024-03-01 12:13:05 +0200

    First Push

---
+++ website/resources/views/contact-us.blade.php
@@ -0,0 +1,112 @@
+@include('layouts.head')
+<div id="featured-title" class="clearfix featured-title-left">
+    <div id="featured-title-inner" class="wprt-container clearfix">
+        <div class="featured-title-inner-wrap">
+            <div class="featured-title-heading-wrap">
+                <h1 class="featured-title-heading">Contact Us</h1>
+            </div>
+            
+            <div id="breadcrumbs">
+                <div class="breadcrumbs-inner">
+                    <div class="breadcrumb-trail">
+                        <span property="itemListElement" typeof="ListItem">
+                        <a property="item" typeof="WebPage" title="Vainservices" href="{{ url('/') }}" class="main-home" ><span property="name">vainservices</span></a><meta property="position" content="1"></span> &gt; <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Vainservices" href="{{ url('/') }}" class="home" ><span property="name">vainservices limited</span></a><meta property="position" content="2"></span> &gt; <span property="itemListElement" typeof="ListItem"><span property="name" class="post post-page current-item">Contact Us</span><meta property="url" content="https://wpdemo2.vegatheme.com/autoser/contact-us/"><meta property="position" content="3">
+                        </span>                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+<section class="wpb-content-wrapper"><section class="vc_rows wpb_row vc_row-fluid vc_custom_1532679262660"><div class="container"><div class="row"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="wpb_gmaps_widget wpb_content_element"><div class="wpb_wrapper"><div class="wpb_map_wraper"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2470.613400801876!2d-73.99351567683284!3d40.71442663030203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xeb6b213e26767bde!2sMuseum+at+Eldridge+Street!5e0!3m2!1svi!2s!4v1503649112256" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe></div></div></div></div></div></div></div></div><div class="clearfix"></div></section><section class="vc_rows wpb_row vc_row-fluid vc_custom_1532596692360"><div class="container"><div class="row"><div class="wpb_column vc_column_container vc_col-sm-12 vc_col-md-4"><div class="vc_column-inner"><div class="wpb_wrapper">
+    <div class="wprt-headings style-1 clearfix text-left ">
+        <h3 class="heading " >Get In Touch</h3>
+                <div class="sep clearfix"></div>
+        
+                <div class="sub-heading clearfix"><p>Get in touch with the professionals at Vain services for quality services at competitive prices.</p>
+                </div>
+            </div>
+        
+<div class="vc_empty_space"   style="height: 40px"><span class="vc_empty_space_inner"></span></div>
+    <div class="wprt-icon-box style-5 clearfix icon-left w50 accent-outline outline-type align-left rounded-100 has-width ">
+        <div class="icon-wrap">
+            <i class="rt-icon-placeholder2"></i>
+        </div>
+        <h3 class="heading">ADDRESS:</h3>
+        <div class="desc"><p>Mpelembe house flourth Suite 417.</p>
+</div>
+    </div>
+
+<div class="vc_empty_space"   style="height: 25px"><span class="vc_empty_space_inner"></span></div>
+    <div class="wprt-icon-box style-5 clearfix icon-left w50 accent-outline outline-type align-left rounded-100 has-width ">
+        <div class="icon-wrap">
+            <i class="rt-icon-alarm-clock"></i>
+        </div>
+        <h3 class="heading">BUSINESS HOURS:</h3>
+        <div class="desc"><p>Monday &#8211; Friday: 8:00 AM &#8211; 6:00 PM<br />
+Saturday: 8:00 AM &#8211; 3:00 PM</p>
+</div>
+    </div>
+
+<div class="vc_empty_space"   style="height: 25px"><span class="vc_empty_space_inner"></span></div>
+    <div class="wprt-icon-box style-5 clearfix icon-left w50 accent-outline outline-type align-left rounded-100 has-width ">
+        <div class="icon-wrap">
+            <i class="rt-icon-chat"></i>
+        </div>
+        <h3 class="heading">CALL US:</h3>
+        <div class="desc">
+            <p>
+                Phone: +260 770 329 994
+             {{-- &#8211; Fax: 718-999-3979 --}}
+        </p>
+        </div>
+    </div>
+
+</div></div></div><div class="wpb_column vc_column_container vc_col-sm-12 vc_col-md-8"><div class="vc_column-inner"><div class="wpb_wrapper">
+	<div class="wpb_text_column wpb_content_element " >
+		<div class="wpb_wrapper">
+			<div class="wprt-spacer clearfix" style="height: 35px;" data-desktop="0" data-mobi="35" data-smobi="35"></div>
+
+		</div>
+	</div>
+
+<div class="wpcf7 no-js" id="wpcf7-f94-p68-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+<form action="https://wpdemo2.vegatheme.com/autoser/contact-us/#wpcf7-f94-p68-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
+<div style="display: none;">
+<input type="hidden" name="_wpcf7" value="94" />
+<input type="hidden" name="_wpcf7_version" value="5.8" />
+<input type="hidden" name="_wpcf7_locale" value="en_US" />
+<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f94-p68-o1" />
+<input type="hidden" name="_wpcf7_container_post" value="68" />
+<input type="hidden" name="_wpcf7_posted_data_hash" value="" />
+</div>
+<div class="wprt-contact-form style-1 margin-top-10">
+	<p><span class="wpcf7-form-control-wrap" data-name="your-name"><input size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name *" value="" type="text" name="your-name" /></span><br />
+<span class="wpcf7-form-control-wrap" data-name="your-email"><input size="40" class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Email *" value="" type="email" name="your-email" /></span><br />
+<span class="wpcf7-form-control-wrap" data-name="your-subject"><input size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Subject *" value="" type="text" name="your-subject" /></span><br />
+<span class="wpcf7-form-control-wrap" data-name="your-message"><textarea cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Message *" name="your-message"></textarea></span><br />
+<input class="wpcf7-form-control wpcf7-submit has-spinner submit" type="submit" value="Send Message" />
+	</p>
+</div><div class="wpcf7-response-output" aria-hidden="true"></div>
+</form>
+</div>
+</div></div></div></div></div><div class="clearfix"></div></section><section class="vc_rows wpb_row vc_row-fluid bg-accent"><div class="container"><div class="row"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
+    <div class="wprt-action-box style-1 has-icon ">
+        <div class="inner">
+            <div class="heading-wrap">
+                <div class="text-wrap">
+                    <h3 class="heading">Fixed Price MOT Test includes a Comprehensive Safety Check</h3>
+                    <span class="icon"><i class="as-icon-car-parts-6"></i></span> 
+                </div>
+            </div>
+            <div class="button-wrap">
+                {{-- <a class="wprt-button white rounded-3px" href="../appointment/index.html" target="_self">BOOK YOUR MOT</a>             --}}
+            </div>
+        </div>
+    </div>
+
+</div></div></div></div></div><div class="clearfix"></div></section>
+</section>  
+@include('layouts.footer')                
---
+++ website/resources/views/layouts/footer.blade.php
@@ -0,0 +1,188 @@
+<footer id="footer">
+    <div id="footer-widgets" class="container">
+        <div class="row">
+<div class="col-md-3 res-m-bttm">
+<div id="media_image-1" class="widget footer-widget widget_media_image">
+    <img width="202" height="52" src="public/pics/logo.jpeg" class="image wp-image-37  attachment-full size-full" alt="" style="max-width: 100%; height: auto;" decoding="async" loading="lazy" /></div><div id="custom_html-1" class="widget_text widget footer-widget widget_custom_html"><div class="textwidget custom-html-widget"><p>We know that honesty and transparency, coupled with reliable and friendly customer service, is what really builds customer trust.</p>
+<div class="socials clearfix">
+<div class="icon"><a target="_blank" href="#" rel="noopener"><i class="rt-icon-facebook"></i></a></div>
+{{-- <div class="icon"><a target="_blank" href="#" rel="noopener"><i class="rt-icon-twitter"></i></a></div> --}}
+{{-- <div class="icon"><a target="_blank" href="#" rel="noopener"><i class="rt-icon-pinterest"></i></a></div> --}}
+<div class="icon"><a target="_blank" href="#" rel="noopener"><i class="rt-icon-linkedin"></i></a></div>
+{{-- <div class="icon"><a target="_blank" href="#" rel="noopener"><i class="rt-icon-instagram"></i></a></div> --}}
+</div></div></div>	</div><!-- end col-lg-3 -->
+
+<div class="col-md-3 res-m-bttm">
+<div id="custom_html-2" class="widget_text widget footer-widget widget_custom_html"><h2 class="widget-title"><span>COMPANY LINKS</span></h2><div class="textwidget custom-html-widget"><ul class="links clearfix col2">
+<li class="style-2"><a href="{{ route('about') }}">About Us</a></li>
+{{-- <li class="style-2"><a href="#">Services</a></li>
+<li class="style-2"><a href="#">Awards</a></li>
+<li class="style-2"><a href="#">Our Offers</a></li>
+<li class="style-2"><a href="#">Community</a></li>
+<li class="style-2"><a href="#">History</a></li>
+<li class="style-2"><a href="#">Events</a></li>
+<li class="style-2"><a href="#">Offices</a></li>
+<li class="style-2"><a href="#">FAQs</a></li>
+<li class="style-2"><a href="#">Our Gallery</a></li> --}}
+<li class="style-2"><a href="{{ route('contact') }}">Contact Us</a></li>
+{{-- <li class="style-2"><a href="#">Shop</a></li> --}}
+</ul></div></div>	</div><!-- end col-lg-3 -->
+
+<div class="col-md-3 res-m-bttm">
+{{-- <div id="media_gallery-1" class="widget footer-widget widget_media_gallery"><h2 class="widget-title"><span>Gallery</span></h2><div id='gallery-1' class='gallery galleryid-518 gallery-columns-3 gallery-size-full'><figure class='gallery-item'>
+    <div class='gallery-icon landscape'>
+        <img width="164" height="164" src="public/wp-content/uploads/sites/2/2018/07/ins1.jpg" class="attachment-full size-full" alt="" decoding="async" loading="lazy" srcset="https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins1.jpg 164w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins1-150x150.jpg 150w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins1-70x70.jpg 70w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins1-100x100.jpg 100w" sizes="(max-width: 164px) 100vw, 164px" />
+    </div></figure><figure class='gallery-item'>
+    <div class='gallery-icon landscape'>
+        <img width="164" height="164" src="public/wp-content/uploads/sites/2/2018/07/ins2.jpg" class="attachment-full size-full" alt="" decoding="async" loading="lazy" srcset="https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins2.jpg 164w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins2-150x150.jpg 150w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins2-70x70.jpg 70w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins2-100x100.jpg 100w" sizes="(max-width: 164px) 100vw, 164px" />
+    </div></figure><figure class='gallery-item'>
+    <div class='gallery-icon landscape'>
+        <img width="164" height="164" src="public/wp-content/uploads/sites/2/2018/07/ins3.jpg" class="attachment-full size-full" alt="" decoding="async" loading="lazy" srcset="https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins3.jpg 164w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins3-150x150.jpg 150w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins3-70x70.jpg 70w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins3-100x100.jpg 100w" sizes="(max-width: 164px) 100vw, 164px" />
+    </div></figure><figure class='gallery-item'>
+    <div class='gallery-icon landscape'>
+        <img width="164" height="164" src="public/wp-content/uploads/sites/2/2018/07/ins4.jpg" class="attachment-full size-full" alt="" decoding="async" loading="lazy" srcset="https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins4.jpg 164w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins4-150x150.jpg 150w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins4-70x70.jpg 70w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins4-100x100.jpg 100w" sizes="(max-width: 164px) 100vw, 164px" />
+    </div></figure><figure class='gallery-item'>
+    <div class='gallery-icon landscape'>
+        <img width="164" height="164" src="public/wp-content/uploads/sites/2/2018/07/ins5.jpg" class="attachment-full size-full" alt="" decoding="async" loading="lazy" srcset="https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins5.jpg 164w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins5-150x150.jpg 150w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins5-70x70.jpg 70w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins5-100x100.jpg 100w" sizes="(max-width: 164px) 100vw, 164px" />
+    </div></figure><figure class='gallery-item'>
+    <div class='gallery-icon landscape'>
+        <img width="164" height="164" src="public/wp-content/uploads/sites/2/2018/07/ins6.jpg" class="attachment-full size-full" alt="" decoding="async" loading="lazy" srcset="https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins6.jpg 164w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins6-150x150.jpg 150w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins6-70x70.jpg 70w, https://wpdemo2.vegatheme.com/autoser/wp-content/uploads/sites/2/2018/07/ins6-100x100.jpg 100w" sizes="(max-width: 164px) 100vw, 164px" />
+    </div></figure>
+</div>
+</div>	 --}}
+</div>
+
+<!-- end col-lg-3 -->
+
+<div class="col-md-3 res-m-bttm">
+<div id="custom_html-3" class="widget_text widget footer-widget widget_custom_html"><h2 class="widget-title"><span>CONTACT INFO</span></h2><div class="textwidget custom-html-widget"><ul class="info">
+<li class="address clearfix">
+<div class="inner">
+    <span class="hl">Address:</span>
+    <span class="text">Mpelembe house flourth Suite 417.</span>
+</div>
+</li>
+<li class="phone clearfix">
+<div class="inner">
+    <span class="hl">Phone:</span> 
+    <span class="text">+260 770 329 994</span>
+</div>
+</li>
+<li class="email clearfix">
+<div class="inner">
+    <span class="hl">E-mail:</span>
+    <span class="text">info@vainserviceslimited.com</span>
+</div>
+</li>
+{{-- <li class="email clearfix">
+<div class="inner">
+    <span class="hl">Skype:</span>
+    <span class="text">help@autoservice.com</span>
+</div>
+</li> --}}
+</ul></div></div>	</div><!-- end col-lg-3 -->
+
+
+        </div>
+    </div>
+</footer>
+        
+        <div id="bottom" class="clearfix style-1">
+    <div id="bottom-bar-inner" class="wprt-container">
+        <div class="bottom-bar-inner-wrap">
+            <div class="bottom-bar-content">
+                <div id="copyright">
+                    &copy; Vainservices - Made by Beeweb with Love.                        </div>
+            </div>
+
+            <div class="bottom-bar-menu">
+                <ul id="menu-footer-menu" class="bottom-nav"><li id="menu-item-706" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-706"><a href="{{ route('home') }}">Home</a></li>
+<li id="menu-item-654" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-654"><a href="{{ route('about') }}">About</a></li>
+<li id="menu-item-655" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-655"><a href="{{ route('contact') }}">Contact</a></li>
+</ul>                        
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+<a id="scroll-top"></a>
+
+
+<script>
+    window.RS_MODULES = window.RS_MODULES || {};
+    window.RS_MODULES.modules = window.RS_MODULES.modules || {};
+    window.RS_MODULES.waiting = window.RS_MODULES.waiting || [];
+    window.RS_MODULES.defered = true;
+    window.RS_MODULES.moduleWaiting = window.RS_MODULES.moduleWaiting || {};
+    window.RS_MODULES.type = 'compiled';
+</script>
+<script type="text/html" id="wpb-modifications"> window.wpbCustomElement = 1; </script>	<script type="text/javascript">
+(function () {
+    var c = document.body.className;
+    c = c.replace(/woocommerce-no-js/, 'woocommerce-js');
+    document.body.className = c;
+})();
+</script>
+<link rel='stylesheet' id='rs-plugin-settings-css' href='public/wp-content/plugins/revslider/public/assets/css/rs64261.css?ver=6.6.16' type='text/css' media='all' />
+<style id='rs-plugin-settings-inline-css' type='text/css'>
+#rs-demo-id {}
+</style>
+<script type="text/javascript" src="public/wp-includes/js/jquery/ui/core.min3f14.js?ver=1.13.2" id="jquery-ui-core-js"></script>
+<script type="text/javascript" src="public/wp-includes/js/jquery/ui/datepicker.min3f14.js?ver=1.13.2" id="jquery-ui-datepicker-js"></script>
+<script type="text/javascript" id="jquery-ui-datepicker-js-after">
+/* <![CDATA[ */
+jQuery(function(jQuery){jQuery.datepicker.setDefaults({"closeText":"Close","currentText":"Today","monthNames":["January","February","March","April","May","June","July","August","September","October","November","December"],"monthNamesShort":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"nextText":"Next","prevText":"Previous","dayNames":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"dayNamesShort":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"dayNamesMin":["S","M","T","W","T","F","S"],"dateFormat":"MM d, yy","firstDay":1,"isRTL":false});});
+/* ]]> */
+</script>
+<script type="text/javascript" src="public/wp-content/plugins/booked/assets/js/spin.min7406.js?ver=2.0.1" id="booked-spin-js-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/booked/assets/js/spin.jquery7406.js?ver=2.0.1" id="booked-spin-jquery-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/booked/assets/js/tooltipster/js/jquery.tooltipster.min9b70.js?ver=3.3.0" id="booked-tooltipster-js"></script>
+<script type="text/javascript" id="booked-functions-js-extra">
+/* <![CDATA[ */
+var booked_js_vars = {"ajax_url":"https:\/\/wpdemo2.vegatheme.com\/autoser\/wp-admin\/admin-ajax.php","profilePage":"","publicAppointments":"","i18n_confirm_appt_delete":"Are you sure you want to cancel this appointment?","i18n_please_wait":"Please wait ...","i18n_wrong_username_pass":"Wrong username\/password combination.","i18n_fill_out_required_fields":"Please fill out all required fields.","i18n_guest_appt_required_fields":"Please enter your name to book an appointment.","i18n_appt_required_fields":"Please enter your name, your email address and choose a password to book an appointment.","i18n_appt_required_fields_guest":"Please fill in all \"Information\" fields.","i18n_password_reset":"Please check your email for instructions on resetting your password.","i18n_password_reset_error":"That username or email is not recognized.","nonce":"00f8a3d6f4"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="public/wp-content/plugins/booked/assets/js/functionsfc3c.js?ver=2.4.4" id="booked-functions-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/contact-form-7/includes/swv/js/index41a3.js?ver=5.8" id="swv-js"></script>
+<script type="text/javascript" id="contact-form-7-js-extra">
+/* <![CDATA[ */
+var wpcf7 = {"api":{"root":"https:\/\/wpdemo2.vegatheme.com\/autoser\/wp-json\/","namespace":"contact-form-7\/v1"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="public/wp-content/plugins/contact-form-7/includes/js/index41a3.js?ver=5.8" id="contact-form-7-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/revslider/public/assets/js/rbtools.min4261.js?ver=6.6.16" defer async id="tp-tools-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/revslider/public/assets/js/rs6.min4261.js?ver=6.6.16" defer async id="revmin-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min956a.js?ver=2.1.4-wc.8.1.1" id="js-cookie-js"></script>
+<script type="text/javascript" id="woocommerce-js-extra">
+/* <![CDATA[ */
+var woocommerce_params = {"ajax_url":"\/autoser\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/autoser\/?wc-ajax=%%endpoint%%"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="public/wp-content/plugins/woocommerce/assets/js/frontend/woocommerce.min12c8.js?ver=8.1.1" id="woocommerce-js"></script>
+<script type="text/javascript" id="booked-fea-js-js-extra">
+/* <![CDATA[ */
+var booked_fea_vars = {"ajax_url":"https:\/\/wpdemo2.vegatheme.com\/autoser\/wp-admin\/admin-ajax.php","i18n_confirm_appt_delete":"Are you sure you want to cancel this appointment?","i18n_confirm_appt_approve":"Are you sure you want to approve this appointment?"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="public/wp-content/plugins/booked/includes/add-ons/frontend-agents/js/functionsfc3c.js?ver=2.4.4" id="booked-fea-js-js"></script>
+<script type="text/javascript" src="public/wp-includes/js/comment-reply.min84fc.js?ver=6.4.3" id="comment-reply-js" async="async" data-wp-strategy="async"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/plugins84fc.js?ver=6.4.3" id="plugins-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/bootstrap.min84fc.js?ver=6.4.3" id="bootstrap-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/animsition84fc.js?ver=6.4.3" id="animsition-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/countto84fc.js?ver=6.4.3" id="countto-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/typed84fc.js?ver=6.4.3" id="typed-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/vegas84fc.js?ver=6.4.3" id="vegas-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/js_composer/assets/lib/flexslider/jquery.flexslider.min8717.js?ver=7.0" id="flexslider-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/owl.carousel.min84fc.js?ver=6.4.3" id="owl-carousel-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/magnific.popup.min84fc.js?ver=6.4.3" id="magnific-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/cubeportfolio84fc.js?ver=6.4.3" id="cubeportfolio-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/equalize.min84fc.js?ver=6.4.3" id="equalize-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/vendor/shortcodes84fc.js?ver=6.4.3" id="autoser-shortcodes-js"></script>
+<script type="text/javascript" src="public/wp-content/themes/autoser/assets/js/main84fc.js?ver=6.4.3" id="autoser-script-js"></script>
+<script type="text/javascript" src="public/wp-content/plugins/js_composer/assets/js/dist/js_composer_front.min8717.js?ver=7.0" id="wpb_composer_front_js-js"></script>
+<script></script>
+</body>
+
+<!-- Mirrored from wpdemo2.vegatheme.com/autoser/home-3/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 23 Feb 2024 08:59:00 GMT -->
+</html>
